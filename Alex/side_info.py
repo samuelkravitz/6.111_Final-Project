3e3ba781bd4e1d621ca86286445db7544f41b12a
alex abate <a.abate11@gmail.com>
2022-11-20 12:05:48 -0500

    added in the scalefactor parsing and modified side info to maintain only unisnged integers

--- Alex/side_info.py
+++ Alex/side_info.py
@@ -22,56 +22,55 @@ def read_side_information(bitstream, ptr, nchannels):
     else:
         private_bits        = bitstream[ptr:ptr + 3]; ptr += 3;
 
-    scfsi                   = [[] for i in range(nchannels)]        #2D array of bitstreams, [channel][scfsi_band], each item is 1 bit
+
+    #create a bunch of 2d arrays (granules, nchannels) that store unsigned integers
+    scfsi                   = np.zeros(shape=(2,        4), dtype=np.uint16)         #2D array of bitstreams, [channel][scfsi_band], each item is 1 bit
+    part2_3_length          = np.zeros(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel] to index, each item is 12 bits
+    big_values              = np.zeros(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel]. each item is 9 bits
+    global_gain             = np.zeros(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel], each item is 8 bits
+    mixed_block_flag        = np.zeros(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel], each item is 1 bit
+    subblock_gain           = np.zeros(shape=(2,nchannels,3), dtype=np.uint16)       #3D array, [granule, channel, window], each item is 3 bit
+    window_switching_flag   = np.zeros(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel], each item is 1 bit
+    block_type              = np.zeros(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel], each item is 2 bits
+    scalefac_compress       = np.zeros(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel], each item is 4 bits
+    table_select            = np.zeros(shape=(2,nchannels,3), dtype=np.uint16)       #3D array, [granule, channel, region], each item is 5 bits, region may span 2 or 3
+    region0_count           = np.zeros(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel] to index, each item is 4 bits
+    region1_count           = np.zeros(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel] to index, each item is 3 bits
+    preflag                 = np.zeros(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel] to index, each item is 1 bit
+    scalefac_scale          = np.zeros(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel] to index, each item is 1 bit
+    count1table_select      = np.zeros(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel] to index, each item is 1 bit
 
     for ch in range(nchannels):
         for scfsi_band in range(4):
-            scfsi[ch].append(bitstream[ptr]); ptr += 1;
-
-    #create a bunch of 2d arrays (granules, nchannels) that store unsigned integers
-    part2_3_length          = np.empty(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel] to index, each item is 12 bits
-    big_values              = np.empty(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel]. each item is 9 bits
-    global_gain             = np.empty(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel], each item is 8 bits
-    mixed_block_flag        = np.empty(shape=(2,nchannels), dtype=np.uint16)         #2D array, [granule, channel], each item is 1 bit
-    subblock_gain           = np.empty(shape=(2,nchannels,3), dtype=np.uint16)       #3D array, [granule, channel, window], each item is 3 bit
-    window_switching_flag   = np.empty(shape=(2,nchannels), dtype=np.uint16)       #3D array, [granule, channel], each item is 1 bit
-
-    #create a bunch of 2d arrays (granules, nchannels) that store bitstreams
-    scalefac_compress       = [[],[]]     #2D array of bitstreams, [granule][channel], each item is 4 bits
-    block_type              = [[],[]]     #2D array of bitstreams, [granule][channel], each item is 2 bits
-    table_select            = [[[] for i in range(nchannels)],[[] for i in range(nchannels)]]     #3D array of bitstreams, [granule][channel][region], each item is 5 bits
-    region0_count           = [[],[]]     #2D array of bitstreams, [granule][channel], each item is 4 bits
-    region1_count           = [[],[]]     #2D array of bitstreams, [granule][channel], each item is 3 bits
-    preflag                 = [[],[]]     #2D array of bitstreams, [granule][channel], each item is 1 bits
-    scalefac_scale          = [[],[]]     #2D array of bitstreams, [granule][channel], each item is 1 bits
-    count1table_select      = [[],[]]     #2D array of bitstreams, [granule][channel], each item is 1 bits
+            scfsi[ch, scfsi_band]       = int(bitstream[ptr], 2); ptr += 1;
 
     for gr in range(2):
         for ch in range(nchannels):
-            part2_3_length[gr,ch]       = int(bitstream[ptr: ptr + 12], 2); ptr += 12;
-            big_values[gr, ch]          = int(bitstream[ptr: ptr + 9], 2); ptr += 9;
-            global_gain[gr,ch]          = int(bitstream[ptr: ptr + 8], 2); ptr += 8;
+            part2_3_length[gr,ch]           = int(bitstream[ptr: ptr + 12], 2); ptr += 12;
+            big_values[gr, ch]              = int(bitstream[ptr: ptr + 9 ], 2); ptr += 9;
+            global_gain[gr,ch]              = int(bitstream[ptr: ptr + 8 ], 2); ptr += 8;
 
-            scalefac_compress[gr].append(bitstream[ptr: ptr + 4]); ptr += 4;
-            window_switching_flag[gr][ch] = int(bitstream[ptr: ptr + 1],2); ptr += 1;
+            scalefac_compress[gr,ch]        = int(bitstream[ptr: ptr + 4 ], 2); ptr += 4;
+            window_switching_flag[gr][ch]   = int(bitstream[ptr: ptr + 1 ], 2); ptr += 1;
 
             if (window_switching_flag[gr][ch]):
-                block_type[gr].append(bitstream[ptr: ptr + 2]); ptr += 2;
-                mixed_block_flag[gr,ch] = int(bitstream[ptr: ptr + 1], 2); ptr += 1;
+                block_type[gr,ch]           = int(bitstream[ptr: ptr + 2], 2); ptr += 2;
+                mixed_block_flag[gr,ch]     = int(bitstream[ptr: ptr + 1], 2); ptr += 1;
 
                 for region in range(2):
-                    table_select[gr][ch].append(bitstream[ptr: ptr + 5]); ptr += 5;
+                    table_select[gr,ch,region]      = int(bitstream[ptr: ptr + 5], 2); ptr += 5;
                 for window in range(3):
-                    subblock_gain[gr][ch][window] = int(bitstream[ptr: ptr + 3], 2); ptr += 3;
+                    subblock_gain[gr,ch,window]     = int(bitstream[ptr: ptr + 3], 2); ptr += 3;
             else:
                 for region in range(3):
-                    table_select[gr][ch].append(bitstream[ptr: ptr + 5]); ptr += 5;
-                region0_count[gr].append(bitstream[ptr: ptr + 4]); ptr += 4;
-                region1_count[gr].append(bitstream[ptr: ptr + 3]); ptr += 3;
+                    table_select[gr,ch,region]      = int(bitstream[ptr: ptr + 5], 2); ptr += 5;
 
-            preflag[gr].append(bitstream[ptr: ptr + 1]); ptr += 1;
-            scalefac_scale[gr].append(bitstream[ptr: ptr + 1]); ptr += 1;
-            count1table_select[gr].append(bitstream[ptr: ptr + 1]); ptr += 1;
+                region0_count[gr,ch]                = int(bitstream[ptr: ptr + 4], 2); ptr += 4;
+                region1_count[gr,ch]                = int(bitstream[ptr: ptr + 3], 2); ptr += 3;
+
+            preflag[gr,ch]                          = int(bitstream[ptr: ptr + 1], 2); ptr += 1;
+            scalefac_scale[gr,ch]                   = int(bitstream[ptr: ptr + 1], 2); ptr += 1;
+            count1table_select[gr]                  = int(bitstream[ptr: ptr + 1], 2); ptr += 1;
 
     output = {
         "main_data_begin": main_data_begin,
